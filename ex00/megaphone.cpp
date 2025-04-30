0aaebff4058a9b4cf0bc761f56e8a4bfa35c4924
lmatkows <lmatkows@student.42perpignan.fr>
2025-04-30 14:36:48 +0200

    decouverte du cpp

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string arg = argv[i];
+			for (std::string::size_type j = 0; j < arg.length(); j++)
+				std::cout << (char)std::toupper(arg[j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
